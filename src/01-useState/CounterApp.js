07f4ac4e86a6902df6f51ecf984adfc6fc8bb639
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-18 13:39:52 +0200

    add three counters and just 1 adding +1

--- src/01-useState/CounterApp.js
+++ src/01-useState/CounterApp.js
@@ -5,16 +5,32 @@ function CounterApp() {
 
     // State
 
-    const [counter, setCounter] = useState(10);
+    const [{ counter1, counter2, counter3 }, setCounter] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+    });
+
+    console.log(counter1);
+    console.log(counter2);
+    console.log(counter3);
 
 
     return (
         <>
-            <h1>Counter: {counter}</h1>
+            <h1>Counter: {counter1}</h1>
+            <h1>Counter: {counter2}</h1>
+            <h1>Counter: {counter3}</h1>
 
             <hr />
 
-            <button className='btn' onClick={() => setCounter(counter + 1)}    >+1</button>
+            <button className='btn' onClick={
+                () => setCounter({
+                    counter1: counter1 + 1,
+                    counter2: counter2,
+                    counter3: counter3,
+                })
+            }>+1</button>
 
         </>
     )
