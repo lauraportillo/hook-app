40adc21b5e02f4b3a3fd4aeeb93da29a6426a6f3
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-30 17:02:49 +0200

    add react router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
+// REACT ROUTER
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
+
+
+// REACT STRICT MODE
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
