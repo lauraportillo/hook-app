7b3381fb5e849b416b037f70a43ee88a5a943bdc
Laura Portillo <lauraportillo.ark@gmail.com>
2022-07-30 16:47:43 +0200

    add usecontext section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //React
 import React from 'react';
-import TodoApp from './08-useReducer/TodoApp';
 //Components
 // import CounterApp from './01-useState/CounterApp';
 // import CounterWithCustomHook from './01-useState/CounterWithCustomHook';
@@ -14,6 +13,8 @@ import TodoApp from './08-useReducer/TodoApp';
 // import CallbackHook from './06-memos/CallbackHook';
 // import { Padre } from './07-tarea-memo/Padre';
 // import './08-useReducer/intro-reducer';
+// import TodoApp from './08-useReducer/TodoApp';
+import MainApp from './09-useContext/MainApp';
 
 //Style
 import './stylesheets/App.css';
@@ -33,7 +34,8 @@ const App = () => {
       {/* <MemoHook /> */}
       {/* <CallbackHook /> */}
       {/* <Padre /> */}
-      <TodoApp />
+      {/* <TodoApp /> */}
+      <MainApp />
     </>
 
   )
